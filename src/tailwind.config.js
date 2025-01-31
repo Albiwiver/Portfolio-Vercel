d1989cecff7573ed49538ad1591d086c3541dcac
Albiwiver <albiwiber@gmail.com>
2025-01-31 00:30:07 +0000

    first commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/*.{html,js}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+        mono: ["IBM Plex Mono", "monospace"],
+      },},
+    },
+    plugins: [],
+  }
